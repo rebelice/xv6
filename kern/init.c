1d2da3159603cef026e718d03cbd57f30a8ab7d8
rebelice <yangrebelice@gmail.com>
2019-07-24 10:27:08 +0800

    lab1 sourc code

---
+++ kern/init.c
@@ -0,0 +1,65 @@
+#include <sys/stdio.h>
+#include <sys/string.h>
+#include <sys/assert.h>
+
+#include <kern/console.h>
+
+void i386_init()
+{
+    extern char edata[], end[];
+
+    memset(edata, 0, end - edata);
+
+    cons_init();
+
+    cprintf("Hello world!\n");
+
+    while (1);
+}
+
+/*
+ * Variable panicstr contains argument to first call to panic; used as flag
+ * to indicate that the kernel has already called panic.
+ */
+const char *panicstr;
+
+/*
+ * Panic is called on unresolvable fatal errors.
+ * It prints "panic: mesg", and then enters the kernel monitor.
+ */
+void
+_panic(const char *file, int line, const char *fmt,...)
+{
+	va_list ap;
+
+	if (panicstr)
+		goto dead;
+	panicstr = fmt;
+
+	// Be extra sure that the machine is in as reasonable state
+	asm volatile("cli; cld");
+
+	va_start(ap, fmt);
+	cprintf("kernel panic at %s:%d: ", file, line);
+	vcprintf(fmt, ap);
+	cprintf("\n");
+	va_end(ap);
+
+dead:
+	/* break into the kernel monitor */
+	while (1)
+		monitor(NULL);
+}
+
+/* like panic, but don't */
+void
+_warn(const char *file, int line, const char *fmt,...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	cprintf("kernel warning at %s:%d: ", file, line);
+	vcprintf(fmt, ap);
+	cprintf("\n");
+	va_end(ap);
+}
