5b060f9edbbfc1f5f16403282e8592169292305a
rebelice <yangrebelice@gmail.com>
2019-07-30 21:07:47 +0800

    Add simple code

---
+++ simple/bye.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void bye(char *A)
+{
+	printf("Bye,%s\n", A);
+}
---
+++ simple/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern void bye(char *s);
+
+int main(void* argc, void** argv)
+{
+	printf("Hello, %s\n", argv[1]);
+	bye(argv[1]);
+	return 0;
+}
