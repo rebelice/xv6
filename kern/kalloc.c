a4efb739fff914f2289c8da7c2b975f65ab42340
rebelice <yangrebelice@gmail.com>
2019-08-22 13:09:16 +0800

    lab2 v1.1 kalloc.c with comments

--- kern/kalloc.c
+++ kern/kalloc.c
@@ -1,3 +1,7 @@
+// Physical memory allocator, intended to allocate memory for
+// user processes, kernel stacks, page table pages, and pipe
+// buffers. Allocates 4096-byte pages.
+
 #include <inc/types.h>
 #include <inc/mmu.h>
 #include <inc/memlayout.h>
@@ -8,16 +12,21 @@
 #include <kern/kalloc.h>
 #include <kern/console.h>
 
+// First address after kernel loaded from ELF file defined by the
+// kernel linker script in kernel.ld.
 extern char end[];
 
+// Free page's list element struct.
+// We store each free page's run structure in the free page itself.
 struct run {
 	struct run *next;
 };
 
 struct {
-	struct run *free_list;
+	struct run *free_list; // Free list of physical pages
 } kmem;
 
+// Free the page of physical memory pointed at by v.
 void
 kfree(char *v)
 {
@@ -26,6 +35,7 @@ kfree(char *v)
 	if ((uint32_t)v % PGSIZE || v < end || V2P(v) >= PHYSTOP)
 		panic("kfree");
 
+	// Fill with junk to catch dangling refs.
 	memset(v, 1, PGSIZE);
 
 	r = (struct run *)v;
@@ -42,6 +52,11 @@ free_rage(void *vstart, void *vend)
 		kfree(p);
 }
 
+// Initialization happens in two phases.
+// 1. Call boot_alloc_init() whie still using entrypgdir to place just
+// the pages mapped by entrypgdir on free list.
+// 2. Call alloc_init() with the rest of the physical pages after
+// installing a full page table.
 void
 boot_alloc_init(void)
 {
@@ -55,6 +70,9 @@ alloc_init(void)
 	free_rage(P2V(4*1024*1024), P2V(PHYSTOP));
 }
 
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
 char *
 kalloc(void)
 {
